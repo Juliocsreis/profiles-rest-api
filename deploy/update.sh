de8a107c305ded2b2cd55e37f578b5070ed3bc12
Julio <juliocsreis@gmail.com>
2019-07-29 11:54:17 -0300

    fixing bug to run server

--- deploy/update.sh
+++ deploy/update.sh
@@ -8,6 +8,6 @@ git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart profiles_api
-supervisorctl restart profiles_project
+
 
 echo "DONE! :)"
